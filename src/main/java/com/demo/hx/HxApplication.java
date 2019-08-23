82c5315b9dd6259aa868352710e738e43a3cadc0
reta <549585103@qq.com>
2019-08-23 23:06:25 +0800

    加了一行备注

--- src/main/java/com/demo/hx/HxApplication.java
+++ src/main/java/com/demo/hx/HxApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HxApplication {
-
+//测试提交代码
 	public static void main(String[] args) {
         System.out.println("");
 		SpringApplication.run(HxApplication.class, args);
